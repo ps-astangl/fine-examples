86aa321da453a7e00fde6927803d01beb1faa35d
ps-astangl <astangl@leaporbit.com>
2020-08-07 07:51:16 -0400

    Initial Commit For Mediator Example

---
+++ Mediator/Controllers/ExampleController.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mediator.Controllers
+{
+    [ApiController]
+    [Route("/api/")]
+    [Route("[controller]")]
+    public class ExampleController : ControllerBase
+    {
+        [HttpGet, Route("[action]")]
+        public async Task<IActionResult> Patient()
+        {
+            return await Task.Factory.StartNew(() => Ok("Hello World"));
+        }
+    }
+}
